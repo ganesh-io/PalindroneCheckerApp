6d920e426dc7d91e73fe908f074558640f4ae51a
ganesh-io <saisanagaram@gmail.com>
2026-03-06 14:29:03 +0530

    Implemented UC8 Linked List Palindrome Checker

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,39 +1,39 @@
-import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
-
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
+    // Node class for Linked List
+    static class Node {
+        char data;
+        Node next;
 
-        // UC7 Logic: Deque (Double Ended Queue)
-        Deque<Character> deque = new ArrayDeque<>();
-
-        // Add each character to the deque
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
+        Node(char data) {
+            this.data = data;
+            this.next = null;
         }
+    }
 
-        boolean isPalindrome = true;
+    // Convert string to linked list
+    public static Node createLinkedList(String str) {
+        Node head = null, temp = null;
 
-        // Compare from both ends until 0 or 1 element remains
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                isPalindrome = false;
-                break;
+        for(char c : str.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if(head == null) {
+                head = newNode;
+                temp = newNode;
+            } else {
+                temp.next = newNode;
+                temp = newNode;
             }
         }
+        return head;
+    }
 
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome.");
-        } else {
-            System.out.println(input + " is not a Palindrome.");
-        }
+    public static void main(String[] args) {
+        String input = "madam";
 
-        sc.close();
+        Node head = createLinkedList(input);
+
+        System.out.println("Linked List created for: " + input);
     }
 }
